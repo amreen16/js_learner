1de09d06daaa54b436daa8f4e24a4185550d3099
amreen16 <148866812+amreen16@users.noreply.github.com>
2023-10-25 06:30:05 +0000

    conv of datatypes

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,15 @@
-console.log("Bismillah")
+/*
+let age=true
+console.log(typeof age)
+console.log(typeof (age))
+let inNum=Number(age)
+console.log(typeof inNum)
+console.log(inNum)
+
+let logIn="am"
+let res=Boolean(logIn)
+console.log(res)
+*/
+let num=30
+let char=String(num)
+console.log(char)
