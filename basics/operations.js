13846d8376051be8edbfdf509f3b4897d22ebbcb
amreen16 <148866812+amreen16@users.noreply.github.com>
2023-10-25 09:42:31 +0000

    conversion operations

---
+++ basics/operations.js
@@ -0,0 +1,19 @@
+let str1="shaik"
+let str2=" Amreen"
+let str3=str1+str2
+/*
+console.log(str3)
+console.log("1"+ 2)//12
+console.log("1"+"2")//12
+console.log("1"+2+2)//122
+console.log("1"+(2+2))//14
+console.log(2+2)//4
+console.log(1+2+"2")//32
+console.log(true)//true
+console.log(+true)//1
+console.log(+"true")//NaN
+console.log(+"")//0
+let inc=100
+inc++;
+console.log(inc)
+*/
