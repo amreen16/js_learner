2c6fad9390309db4922137a7bd6b635448967904
amreen16 <148866812+amreen16@users.noreply.github.com>
2023-10-30 04:40:27 +0000

    func

--- basics/functions.js
+++ basics/functions.js
@@ -12,3 +12,34 @@ function jobOffer(name){
     return `Congratulations! ${name}, You are selected`
 }
 //console.log(jobOffer());
+
+function class1(){
+    let sub1="eng";
+    function class2(){
+        let sub2="maths";
+        //console.log(sub1)
+    }
+   //console.log(sub2);
+    class2()
+}
+class1()
+
+if(true){
+    const emp="amreen"
+    if(emp==="amreen"){
+        const team=" Gov"
+        //console.log(emp +  team)
+    }
+    //console.log(team)
+}
+
+function add(num){
+    return num+1;
+}
+//console.log(add(9));
+
+//let sub=function(num){
+   // return num-1;
+//}
+//console.log(sub)
+
